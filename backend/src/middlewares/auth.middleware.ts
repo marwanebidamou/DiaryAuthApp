ece17d61c39d4bc88955032ef8f6ec9684a0ecf9
Marwane Bidamou <devr.marwane@gmail.com>
2024-12-05 16:43:54 -0600

    backend project: add authenticate middleware

---
+++ backend/src/middlewares/auth.middleware.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import { JwtPayload } from "jsonwebtoken";
+import { verifyToken } from "../utils/jwt.util";
+
+
+export const authenticate = (req: Request, res: Response, next: NextFunction) => {
+
+    try {
+        // Get the token from the Authorization header
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            res.status(401).json({ message: 'Unauthorized: No token provided' });
+            return;
+        }
+
+        const token = authHeader.split(' ')[1]; // Extract the token
+
+        // Verify the token
+        const decoded = verifyToken(token);
+
+        // Proceed to the next middleware or route handler
+        next();
+
+    } catch {
+        res.status(401).json({ error: 'Unauthorized. Invalid token.' });
+    }
+}
--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
 import { signInAction, signUpAction } from '../controllers/auth.controller';
+import { authenticate } from '../middlewares/auth.middleware';
 
 const authRouter = Router();
 
 authRouter.post('/sign-up', signUpAction);
 authRouter.post('/sign-in', signInAction);
+
+authRouter.get('/refresh', authenticate, (req, res) => {
+
+    res.json({ authenticated: true });
+
+});
+
 
 
 
