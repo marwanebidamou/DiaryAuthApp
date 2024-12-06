b7007170ff326eb248c9ae469b6044b62db50162
Marwane Bidamou <devr.marwane@gmail.com>
2024-12-06 00:11:05 -0600

    frontend project: implement login component logic

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatCard, MatCardContent, MatCardTitle } from '@angular/material/card';
+import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { LoginRequestDTO } from '../../models/auth.dtos';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatCard, MatCardTitle, MatCardContent, MatFormField, MatButton, MatLabel, MatError, MatInputModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  error = signal('');
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  form = inject(FormBuilder).group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+
+  });
+
+  get email() { return this.form.controls.email };
+  get password() { return this.form.controls.password };
+
+  onSubmit() {
+
+    const data: LoginRequestDTO = { email: this.email.value!, password: this.password.value! };
+
+    this.authService.login(data).subscribe({
+      next: () => this.router.navigate(['/diary']),
+      error: () => (this.error.set('Invalid email or password')),
+    });
+  }
 }
