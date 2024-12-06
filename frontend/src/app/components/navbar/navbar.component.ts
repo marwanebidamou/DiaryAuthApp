853a3d7c879c7462d2cb49135712253ff5d1e916
Marwane Bidamou <devr.marwane@gmail.com>
2024-12-06 00:10:12 -0600

    frontend project:  refactor navbar => show actions depending on guest / connected user

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -11,5 +12,16 @@ import { RouterLink } from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  private authService = inject(AuthService);
 
+  user = computed(() => this.authService.currentUser());
+
+  constructor() {
+
+  }
+
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
