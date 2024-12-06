1f1613aeb8ac80ffc72df8a3e7af42dfe70422fb
Marwane Bidamou <devr.marwane@gmail.com>
2024-12-06 00:24:54 -0600

    frontend project: add and apply CanActivate auth guard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
     { path: 'login', loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent) },
     { path: 'signup', loadComponent: () => import('./components/signup/signup.component').then(m => m.SignupComponent) },
-    { path: 'diary', loadComponent: () => import('./components/diaries/diaries.component').then(m => m.DiariesComponent) },
+    {
+        path: 'diary',
+        loadComponent: () => import('./components/diaries/diaries.component').then(m => m.DiariesComponent),
+        canActivate: [authGuard]
+    },
     { path: '**', redirectTo: 'home' },
 
 ];
